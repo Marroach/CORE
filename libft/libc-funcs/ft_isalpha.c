592e7a05f34158e8b0fd68228f35faf586813e3a
Marian Saldana <marisald@c2r8s3.42prague.com>
2024-09-23 14:02:03 +0200

    jgjkjd

--- libft/libc-funcs/ft_isalpha.c
+++ libft/libc-funcs/ft_isalpha.c
@@ -19,35 +19,30 @@ int		ft_isalpha(int c)
 
 // Test function to compare ft_isalpha with isalpha
 void test_isalpha(int c) {
-    // Testing with ft_isalpha
-    int ft_result = ft_isalpha(c);
-    // Testing with standard isalpha
+        int ft_result = ft_isalpha(c);
     int std_result = isalpha(c);
 
-    // Print the character and both results
     printf("Character: '%c' (ASCII: %d)\n", c, c);
     printf("ft_isalpha: %d, isalpha: %d\n", ft_result, std_result);
     printf("-----\n");
 }
 
 int main() {
-    // Test lowercase letters
     test_isalpha('a');
     test_isalpha('z');
     
-    // Test uppercase letters
+
     test_isalpha('A');
     test_isalpha('Z');
     
-    // Test digits
+
     test_isalpha('0');
     test_isalpha('9');
-    
-    // Test special characters
+
     test_isalpha('@');
     test_isalpha('[');
     
-    // Test non-printable characters
+
     test_isalpha(10);  // Newline
     test_isalpha(32);  // Space
     
